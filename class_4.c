470254f240b84da9e59c1b9473b862c3fe168f44
Shakhawat Johnny <99986085+Jshakhwat@users.noreply.github.com>
2023-12-31 16:00:41 +0600

    uploading class 4

---
+++ class_4.c
@@ -0,0 +1,112 @@
+#include<stdio.h>
+int main(){
+    /*
+int number;
+
+printf("enter a number: ");
+scanf("%d",&number);
+
+    switch(number){
+
+        case 0:
+            printf("the number is even");
+            break;
+        case !0:
+            printf("the number is odd");
+            break;
+        default:
+            printf("the input is invalid!");
+
+}
+*/
+
+/*
+int main(){
+int number;
+printf("enter a number: ");
+scanf("%d",&number);
+
+    switch(number%2==0){
+
+        case 0:
+            printf("the number is even");
+            break;
+        case 1:
+            printf("the number is odd");
+            break;
+        default:
+            printf("the input is invalid!");
+}
+*/
+
+/*
+int i;
+for (i=101; i<=200; i=i+1)
+{
+    if(i%2!=0)
+        printf("the even number odd: %d\n",i);
+
+    }
+
+*/
+
+/*```````1+2+..+N``````
+int n,i,sum = 0;
+
+printf("enter N number: ");
+scanf("%d",&n);
+
+    for(i=1; i<=n; i++){
+        sum= sum+i;
+
+    }
+     printf("the sum of the series = %d",sum);
+
+*/
+
+/*1^3+2^3+...+n^3````````````````````````
+int n,i,sum = 0;
+
+printf("enter N number: ");
+scanf("%d",&n);
+
+    for(i=1; i<=n; i++){
+
+        sum= sum+pow(i,3);
+
+
+    }
+     printf("the sum of the series = %d",sum);
+
+*/
+/*
+//factorial``````
+int n,i,fact = 1;
+
+printf("enter N number: ");
+scanf("%d",&n);
+
+    for(i=1; i<=n; i++){
+
+        fact= fact*i;
+    }
+    printf("the factorail of the series is = %d",fact);
+
+*/
+int i = 1,n,sum=0;
+printf("enter the value of N: ");
+scanf("%d",&n);
+while(i<=n){
+
+    sum = sum + i;
+    i = i+1;
+    fact =
+}
+printf("%d\n", sum);
+
+
+
+
+return 0;
+
+}
